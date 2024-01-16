3618f503b316a51da72d72f1b7b2f6e174659734
Karl Angelo Oriel <47414060+itsmelrak22@users.noreply.github.com>
2024-01-16 16:22:23 +0800

    first commit

---
+++ footer_content/footer.php
@@ -0,0 +1,32 @@
+<style>
+        /* Custom CSS for the footer */
+        footer {
+            background-color: #000;
+            color: #fff;
+            padding: 20px;
+        }
+        /* Style for footer links */
+        footer a {
+            color: #fff;
+            text-decoration: none;
+            margin-right: 20px;
+        }
+    </style>
+</head>
+<body>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h4>Contact Us</h4>
+                <p>Email: trendythreadsapparel30@gmail.com</p>
+                <p>Phone: 0963-609-9067 or 0951-927-3835</p>
+            </div>
+            <div class="col-md-4">
+                <h4>Facebook Page</h4>
+                <p>Visit our Facebook page for updates and news.</p>
+                <a href="https://www.facebook.com/SlayAndWearItFashionable?mibextid=LQQJ4d" target="_blank">Trendy Threads Apparel Facebook</a>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ pages/home/actions/home_main.php
@@ -0,0 +1,76 @@
+<!-- <div class="container-fluid">
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="C:\xampp\htdocs\Ecommerce2.0\assets\images\11.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="https://scontent.fdvo2-1.fna.fbcdn.net/v/t39.30808-6/274735233_6948219865253216_8374726888643843009_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=19026a&_nc_eui2=AeHEYOoUKDYCnXNRCuz0gFYlaUZoqQxqAbxpRmipDGoBvNypABTxkAt2Qn8FnV1gCTi9bsuW6ExH5ipGGFeJakgM&_nc_ohc=7RD9WUoD85IAX-7RLJB&_nc_oc=AQk7iZqeiP1g7_3p4yVo8aja92H7jA8mNSWk8xPo1JkWxou2NKgKFfprZS_xW3Nc7pU&_nc_ht=scontent.fdvo2-1.fna&oh=00_AfAsBm7FadIVp6hfaIWj4Hm6ESzUsidEaQy6IaGwe_tpHQ&oe=63F119E6" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="https://scontent.fdvo2-2.fna.fbcdn.net/v/t39.30808-6/274723630_6957419100999959_5378234553795738446_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=19026a&_nc_eui2=AeGPX6MoX5bFSPQHM6SiQh676HQCbbaiyqfodAJttqLKp3VVaSYn-lYuE7yzrd-3RyUGI-APHfj68et-mG1MifpL&_nc_ohc=viIVHxtVGMsAX8ZGQax&_nc_ht=scontent.fdvo2-2.fna&oh=00_AfDlx7Fny5yGV1gECskQcD2T9agHfBfqfA10XEkHxY-MXw&oe=63F1D978" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    <div class="card my-2">
+        <div class="card-body">
+            <div class="d-flex">
+                <div class="card mx-2" style="width: '25%';">
+                    <div class="card-body">
+                        <h5 class="card-title">React Native</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">2 Years</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+
+                </div>
+                <div class="card mx-2" style="width: '25%';">
+                    <div class="card-body">
+                        <h5 class="card-title">PHP</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">1 Year</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+
+                </div>
+                <div class="card mx-2" style="width: '25%';">
+                    <div class="card-body">
+                        <h5 class="card-title">Java </h5>
+                        <h6 class="card-subtitle mb-2 text-muted">3 Years</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+
+                </div>
+                <div class="card mx-2" style="width: '25%';">
+                    <div class="card-body">
+                        <h5 class="card-title">React.js</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">2 Years</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</div>
+<script>
+    $(document).ready(function() {
+        to_Navigation()
+    });
+</script>> -->
